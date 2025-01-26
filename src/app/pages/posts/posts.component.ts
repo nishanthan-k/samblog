12472530825e84a028dbfed7671624b596a824b1
nishanthan-k <krn.nishanthan@gmail.com>
2025-01-26 21:48:52 +0530

    feat: post list on home page

--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../../services/post.service';
+import { Post } from '../../models/post.model';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-posts',
-  imports: [],
+  imports: [HttpClientModule, NgFor],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss'
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
+  posts = [] as Post[];
+  constructor(private postService: PostService) {}
 
+  ngOnInit(): void {
+      this.postService.getPosts().subscribe(posts => this.posts = posts);
+  }
 }
